cdd15ef8017b3630fd83213dc024140befc574a0
Redha Boubakour <redha.boubakour@yahoo.fr>
2020-03-30 13:15:51 +0200

    Commit5

--- chalenge1-countingCards.js
+++ chalenge1-countingCards.js
@@ -4,8 +4,8 @@ Having more high cards remaining in the deck favors the player.Each card is assi
 
 Count Change	Cards
 +             1	2, 3, 4, 5, 6
-0	            7, 8, 9
-- 1	          10, 'J', 'Q', 'K', 'A'
+0             7, 8, 9
+- 1           10, 'J', 'Q', 'K', 'A'
 
 You will write a card counting function.It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision(Bet or Hold) should be separated by a single space.
 
@@ -42,7 +42,7 @@ function cc(card) {
 
   var holdbet = "Hold";
   if (count > 0) {
-    holtbet = "Bet";
+    holdbet = "Bet";
   }
 
   return count + " " + holdbet;
