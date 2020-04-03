a404103436e8e3103db254fd3bae9fb49aa2ddd9
Redha Boubakour <redha.boubakour@yahoo.fr>
2020-04-03 10:18:07 +0200

    Commit5

---
+++ home.js
@@ -0,0 +1,15 @@
+console.log("Hello");
+
+// alert("Wait");
+
+var b = "Smooth";
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var name = prompt("What is your name ?");
+var age = prompt("What is your age ?");
+document.getElementById(
+  "someText"
+).innerHTML = `Hey there, my name is ${name} and i'm ${age}`;
